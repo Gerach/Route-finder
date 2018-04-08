6b4a89c06535e4af1f17b0d877a5730ca31dde2b
Gerardas Martynovas <germartas@gmail.com>
2018-04-08 20:23:35 +0300

    MapWindow class introduction - removed all global variables - added MapWindow class and moved navigate_image into it - moved few variabled from main to MapWindow class

--- main.py
+++ main.py
@@ -2,52 +2,61 @@
 
 import cv2
 
-moving = False
-map_x, map_y = 1000, 1000 # top left corner of the map
-window_width, window_height = 1000, 700 # window resolution
-ix, iy = 0, 0 # mouse coordinates before movement
-img_width, img_height = 0, 0
 
+class MapWindow(object):
+    def __init__(self, img, window_name = 'Window'):
+        self.WINDOW_NAME = window_name
+        self.img = img
+        self.img_height, self.img_width, self.channels = img.shape
+        self.map_y, self.map_x = 1000, 1000 # coordinates of top left corner of the map
+        self.window_width, self.window_height = 1000, 700
+        self.mouse_moving = False
+        self.x_before_move, self.y_before_move = 0, 0
 
-def navigate_image(event, x, y, flags, param):
-    global moving, map_x, map_y, ix, iy
-    if event == cv2.EVENT_LBUTTONDOWN:
-        moving = True
-        ix, iy = x, y
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if moving and x >= 0 and y >= 0 and x <window_width and y < window_height:
-            map_x += int((ix - x)/5)
-            map_y += int((iy - y)/5)
+        cv2.namedWindow(self.WINDOW_NAME)
+        cv2.setMouseCallback(self.WINDOW_NAME, self.onMouse)
 
-            if map_x < 0:
-                map_x = 0
-            if map_y < 0:
-                map_y = 0
-            if map_x > img_width - window_width:
-                map_x = img_width - window_width
-            if map_y > img_height - window_height:
-                map_y = img_height - window_height
+        self.redrawImage()
 
-            print(x, y)
-            print(ix, iy)
-    elif event == cv2.EVENT_LBUTTONUP:
-        moving = False
+    def onMouse(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.mouse_moving = True
+            self.x_before_move, self.y_before_move = x, y
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.mouse_moving and x >= 0 and y >= 0 and x < self.window_width and y < self.window_height:
+                self.map_x += int((self.x_before_move - x) / 5)
+                self.map_y += int((self.y_before_move - y) / 5)
+
+                if self.map_x < 0:
+                    self.map_x = 0
+                if self.map_y < 0:
+                    self.map_y = 0
+                if self.map_x > self.img_width - self.window_width:
+                    self.map_x = self.img_width - self.window_width
+                if self.map_y > self.img_height - self.window_height:
+                    self.map_y = self.img_height - self.window_height
+
+                self.redrawImage()
+
+                print(x, y)
+                print(self.x_before_move, self.y_before_move)
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.mouse_moving = False
+
+    def redrawImage(self):
+        img_cropped = self.img[
+                      self.map_y: self.map_y + self.window_height,
+                      self.map_x: self.map_x + self.window_width]
+        cv2.imshow(self.WINDOW_NAME, img_cropped)
 
 
 def main():
-    img = cv2.imread('vilnius.png')
-    global img_height, img_width, channels
-    img_height, img_width, channels = img.shape
-    cv2.namedWindow("Route finder")
-    cv2.setMouseCallback("Route finder", navigate_image)
-
-    while True:
-        img_cropped = img[map_y: map_y + window_height, map_x: map_x + window_width]
-        cv2.imshow('Route finder', img_cropped)
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27:
-            break
-
+    in_file = 'vilnius.png'
+    map = cv2.imread(in_file)
+    window = MapWindow(map, "Route finder")
+    key = -1
+    while key != ord('q') and key != 27 and cv2.getWindowProperty(window.WINDOW_NAME, 0) >= 0:
+        key = cv2.waitKey(1)
     cv2.destroyAllWindows()
 
 
