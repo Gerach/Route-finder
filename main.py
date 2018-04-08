6dd76509328a0cdb396cd1b36b77e89b4d567928
Gerardas Martynovas <germartas@gmail.com>
2018-04-08 23:20:49 +0300

    Small fixes - renamed cv2 to cv - fixed code to meet pep8 requirements - added right mouse button triggers - fixed issue where window would still register movememnt of mouse outside window boundaries

--- main.py
+++ main.py
@@ -1,63 +1,84 @@
 #!/usr/bin/env python3
 
-import cv2
+import cv2 as cv
 
 
 class MapWindow(object):
-    def __init__(self, img, window_name = 'Window'):
+    def __init__(self, img, window_name='Window'):
         self.WINDOW_NAME = window_name
         self.img = img
-        self.img_height, self.img_width, self.channels = img.shape
-        self.map_y, self.map_x = 1000, 1000 # coordinates of top left corner of the map
+        self.img_height, self.img_width = img.shape[:2]
+        self.map_y, self.map_x = 1000, 1000  # coordinates of top left corner of the map
         self.window_width, self.window_height = 1000, 700
         self.mouse_moving = False
+        self.mouse_lb_pressed = False
+        self.mouse_rb_pressed = False
         self.x_before_move, self.y_before_move = 0, 0
 
-        cv2.namedWindow(self.WINDOW_NAME)
-        cv2.setMouseCallback(self.WINDOW_NAME, self.onMouse)
+        cv.namedWindow(self.WINDOW_NAME)
+        cv.setMouseCallback(self.WINDOW_NAME, self.on_mouse)
 
-        self.redrawImage()
+        self.redraw_image()
 
-    def onMouse(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.mouse_moving = True
-            self.x_before_move, self.y_before_move = x, y
-        elif event == cv2.EVENT_MOUSEMOVE:
-            if self.mouse_moving and x >= 0 and y >= 0 and x < self.window_width and y < self.window_height:
-                self.map_x += int((self.x_before_move - x) / 5)
-                self.map_y += int((self.y_before_move - y) / 5)
+    def on_mouse(self, event, x, y, flags, param):
+        if 0 <= x < self.window_width and 0 <= y < self.window_height:
+            if event == cv.EVENT_LBUTTONDOWN:
+                self.mouse_lb_pressed = True
+                self.mouse_moving = True
+                self.x_before_move, self.y_before_move = x, y
+            if event == cv.EVENT_RBUTTONDOWN:
+                self.mouse_rb_pressed = True
+                self.mouse_moving = True
+                self.x_before_move, self.y_before_move = x, y
+            elif event == cv.EVENT_MOUSEMOVE:
+                if self.mouse_moving:
+                    if self.mouse_lb_pressed:
+                        self.map_x += int((self.x_before_move - x) / 5)
+                        self.map_y += int((self.y_before_move - y) / 5)
 
-                if self.map_x < 0:
-                    self.map_x = 0
-                if self.map_y < 0:
-                    self.map_y = 0
-                if self.map_x > self.img_width - self.window_width:
-                    self.map_x = self.img_width - self.window_width
-                if self.map_y > self.img_height - self.window_height:
-                    self.map_y = self.img_height - self.window_height
+                        if self.map_x < 0:
+                            self.map_x = 0
+                        if self.map_y < 0:
+                            self.map_y = 0
+                        if self.map_x > self.img_width - self.window_width:
+                            self.map_x = self.img_width - self.window_width
+                        if self.map_y > self.img_height - self.window_height:
+                            self.map_y = self.img_height - self.window_height
 
-                self.redrawImage()
-
-                print(x, y)
-                print(self.x_before_move, self.y_before_move)
-        elif event == cv2.EVENT_LBUTTONUP:
+                        self.redraw_image()
+                    # elif self.mouse_rb_pressed:
+                    #     multiplier = 0.0001 * abs(y-self.y_before_move)
+                    #     self.img = cv.resize(self.img, None, fx=1+multiplier, fy=1+multiplier, interpolation=cv.INTER_CUBIC)
+                    #
+                    #     self.redraw_image()
+                    #
+                    #     print(y, self.y_before_move, y-self.y_before_move)
+            elif event == cv.EVENT_LBUTTONUP:
+                self.mouse_lb_pressed = False
+                self.mouse_moving = False
+            elif event == cv.EVENT_RBUTTONUP:
+                self.mouse_rb_pressed = False
+                self.mouse_moving = False
+        else:
+            self.mouse_rb_pressed = False
+            self.mouse_lb_pressed = False
             self.mouse_moving = False
 
-    def redrawImage(self):
+    def redraw_image(self):
         img_cropped = self.img[
                       self.map_y: self.map_y + self.window_height,
                       self.map_x: self.map_x + self.window_width]
-        cv2.imshow(self.WINDOW_NAME, img_cropped)
+        cv.imshow(self.WINDOW_NAME, img_cropped)
 
 
 def main():
     in_file = 'vilnius.png'
-    map = cv2.imread(in_file)
-    window = MapWindow(map, "Route finder")
+    img = cv.imread(in_file)
+    window = MapWindow(img, "Route finder")
     key = -1
-    while key != ord('q') and key != 27 and cv2.getWindowProperty(window.WINDOW_NAME, 0) >= 0:
-        key = cv2.waitKey(1)
-    cv2.destroyAllWindows()
+    while key != ord('q') and key != 27 and cv.getWindowProperty(window.WINDOW_NAME, 0) >= 0:
+        key = cv.waitKey(1)
+    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
