984fe814b9baa00303c4e2ce5cf783437cfe51dc
gerach <germartas@gmail.com>
2018-05-09 01:24:59 +0300

    Extend database and its capabilities - introduction of primary and foreign keys - added road and traffic light tables - added function to insert and select missing data

--- database.py
+++ database.py
@@ -10,11 +10,34 @@ class Database(object):
         self.cursor = self.conn.cursor()
 
     def create_tables(self):
-        self.cursor.execute('''CREATE TABLE address(
-                              street text, 
-                              house integer,
-                              x_coord integer,
-                              y_coord integer)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS address(
+                              street TEXT NOT NULL, 
+                              house INTEGER NOT NULL,
+                              x_coord INTEGER NOT NULL,
+                              y_coord INTEGER NOT NULL,
+                              PRIMARY KEY(street, house)
+                              )''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS road(
+                              road_id INTEGER NOT NULL PRIMARY KEY,
+                              street TEXT,
+                              house INTEGER,
+                              speed_limit INTEGER,
+                              x1_coord INTEGER NOT NULL,
+                              y1_coord INTEGER NOT NULL,
+                              x2_coord INTEGER NOT NULL,
+                              y2_coord INTEGER NOT NULL,
+                              FOREIGN KEY (street, house) REFERENCES address(street,house)
+                              )''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS traffic_light(
+                              x_coord INTEGER NOT NULL,
+                              y_coord INTEGER NOT NULL,
+                              road_id INTEGER NOT NULL,
+                              green_light INTEGER,
+                              red_light INTEGER,
+                              PRIMARY KEY (x_coord, y_coord),
+                              FOREIGN KEY (road_id) REFERENCES road(road_id)
+                              )''')
+        self.conn.commit()
 
     def insert_address(self, values):
         try:
@@ -24,9 +47,30 @@ class Database(object):
             print('Invalid argument count given for address insertion.')
             sys.exit(1)
 
+    def insert_road(self, values):
+        try:
+            self.cursor.executemany('INSERT INTO road(street, house, speed_limit, x1_coord, y1_coord, x2_coord, y2_coord) VALUES (?,?,?,?,?,?,?)', values)
+            self.conn.commit()
+        except sqlite3.ProgrammingError:
+            print('Invalid argument count given for road insertion.')
+            sys.exit(1)
+
+    def insert_traffic_light(self, values):
+        try:
+            self.cursor.executemany('INSERT INTO traffic_light VALUES (?,?,?,?,?)', values)
+            self.conn.commit()
+        except sqlite3.ProgrammingError:
+            print('Invalid argument count given for traffic light insertion.')
+            sys.exit(1)
+
     def select_address(self, street, house):
         self.cursor.execute(
             "SELECT x_coord, y_coord FROM address WHERE street = ? AND house = ?", (street, house))
+        return self.cursor.fetchone()
+
+    def select_road(self, road_id):
+        self.cursor.execute(
+            "SELECT * FROM road WHERE road_id = ?", road_id)
         return self.cursor.fetchone()
 
     def show_table_contents(self, table_name):
--- main.py
+++ main.py
@@ -27,14 +27,47 @@ def main():
     dest_address = check_input(sys.argv[2])
 
     db = Database('data.sqlite')
+    # db.create_tables()
     # addresses = [('Naugarduko', 24, 2400, 2870),
     #              ('Didlaukio', 47, 2210, 1020),
     #              ('Sauletekio', 6, 3492, 1124),
     #              ('Antakalnio', 17, 3130, 2114),
     #              ('Saltoniskiu', 29, 2068, 2130),
     #              ]
+    # roads = [('Naugarduko', 24, None, 2374, 2862, 2424, 2832),
+    #          ('Naugarduko', 24, None, 2424, 2832, 2433, 2879),
+    #          ('Naugarduko', 24, None, 2433, 2879, 2379, 2891),
+    #          ('Naugarduko', 24, None, 2379, 2891, 2374, 2862),
+    #          ('Didlaukio', 59, None, 2195, 1042, 2161, 1007),
+    #          ('Sauletekio', 6, None, 3504, 1134, 3487, 1159),
+    #          ('Saltoniskiu', 29, None, 3161, 2121, 3143, 2142),
+    #          (None, None, None, 2374, 2862, 2312, 2900),
+    #          (None, None, None, 2312, 2900, 2278, 2737),
+    #          (None, None, None, 2278, 2737, 2248, 2663),
+    #          (None, None, None, 2248, 2663, 2241, 2629),
+    #          (None, None, None, 2241, 2629, 2238, 2564),
+    #          (None, None, None, 2238, 2564, 2286, 2491),
+    #          (None, None, None, 2286, 2491, 2298, 2452),
+    #          (None, None, None, 2298, 2452, 2313, 2406),
+    #          (None, None, None, 2313, 2406, 2344, 2329),
+    #          (None, None, None, 2344, 2329, 2286, 2266),
+    #          (None, None, None, 2286, 2266, 2238, 2251),
+    #          (None, None, None, 2238, 2251, 2219, 2248),
+    #          (None, None, None, 2219, 2248, 2214, 2083),
+    #          (None, None, None, 2214, 2083, 2118, 2075),
+    #          (None, None, None, 2118, 2075, 2084, 2125),
+    #          (None, None, None, 2374, 2862, 2356, 2776),
+    #          (None, None, None, 2356, 2776, 2344, 2720),
+    #          (None, None, None, 2344, 2720, 2330, 2652),
+    #          (None, None, None, 2330, 2652, 2305, 2642),
+    #          (None, None, None, 2305, 2642, 2248, 2663),
+    #          (None, None, None, 2313, 2406, 2214, 2377),
+    #          (None, None, None, 2214, 2377, 2240, 2276),
+    #          (None, None, None, 2240, 2276, 2238, 2251),
+    #          ('Saltoniskiu', 29, None, 2084, 2125, 2064, 2157),
+    #         ]
     # db.insert_address(addresses)
-    # db.show_table_contents('address')
+    # db.insert_road(roads)
 
     loc_coord = db.select_address(loc_address[0], loc_address[1])
     dest_coord = db.select_address(dest_address[0], dest_address[1])
