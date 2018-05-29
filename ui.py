50124039354efe3447e5022696cf3b45992091a2
Martynovas Gerardas <gerardas.martynovas@mif.stud.vu.lt>
2018-05-29 18:31:25 +0300

    Added address checking (iteration 2 fix)

--- ui.py
+++ ui.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
-
-
-def check_input(value):
-    address = value.rsplit(' ', 1)
-    if len(address) != 2:
-        print('Invalid address format provided.')
-        sys.exit(1)
-    elif address[0] is '' or address[1] is '':
-        print('Invalid address format provided.')
-        sys.exit(1)
-    return address
-
 
 def print_help():
     help_text = [{'command': '?', 'help': 'display help.'},
@@ -27,11 +14,18 @@ def print_help():
         print('\033[91m' + one['command'] + '\033[0m' + ' - ' + one['help'])
 
 
+def check_input(value):
+    address = value.rsplit(' ', 1)
+    if len(address) != 2:
+        return
+    return address
+
+
 class Ui(object):
     def __init__(self, db, map_file, window, route):
         commands = ["exit",
                     "change location",
-                    "change destination"
+                    "change destination",
                     "change location int",
                     "change destination int",
                     "?",
@@ -54,20 +48,35 @@ class Ui(object):
         elif command == '?':
             print_help()
 
-    def change_location(self, map, window, route):
-        input_location = raw_input('Input location address: ')
-        location_address = check_input(input_location)
-        location_coord = self.db.select_address(location_address[0], location_address[1])
+    def check_address(self, direction):
+        input_location = raw_input('Input {} address: '.format(direction))
+        location_coord = None
+        address_exists = False
 
-        window.set_location(map, location_coord)
+        while not address_exists:
+            if not check_input(input_location):
+                print('Invalid address format provided. Correct format : <Street name> <House nr.>')
+                input_location = raw_input('Input {} address: '.format(direction))
+            else:
+                location_coord = self.db.select_address(input_location.rsplit(' ', 1)[0],
+                                                        input_location.rsplit(' ', 1)[1])
+                if not location_coord:
+                    input_location = raw_input('Address not found, try another one: ')
+                else:
+                    address_exists = True
+
+        return input_location, location_coord
+
+    def change_location(self, map_file, window, route):
+        location_address, location_coord = self.check_address('location')
+
+        window.set_location(map_file, location_coord)
         route.set_location(location_address)
 
         window.draw_road(route.get_roads())
 
     def change_destination(self, map_img, window, route):
-        input_destination = raw_input('Input destination address: ')
-        destination_address = check_input(input_destination)
-        destination_coord = self.db.select_address(destination_address[0], destination_address[1])
+        destination_address, destination_coord = self.check_address('destination')
 
         window.set_destination(map_img, destination_coord)
         route.set_destination(destination_address)
